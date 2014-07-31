76b384bfaa2ba91a6ac5e5da9ecaeb2311b7c533
Dekken <dekken@itnet.ie>
2014-07-31 16:14:10 -0700

    mkn profiles / license blurb

--- scene.cpp
+++ scene.cpp
@@ -8,18 +8,18 @@ Copyright (c) 2013, Philip Deegan
 
 This file is part of fragex.
 
-This library is free software; you can redistribute it and/or
+This application is free software; you can redistribute it and/or
 modify it under the terms of the GNU Lesser General Public
 License as published by the Free Software Foundation; either
 version 2.1 of the License, or (at your option) any later version.
 
-This library is distributed in the hope that it will be useful,
+This application is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 Lesser General Public License for more details.
 
 You should have received a copy of the GNU Lesser General Public License
-along with this library.  If not, see <http://www.gnu.org/licenses/>.
+along with this application.  If not, see <http://www.gnu.org/licenses/>.
 */
 #include "scene.hpp"
 
