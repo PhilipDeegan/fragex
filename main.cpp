cfea96d8b3e68562133bec0729efb560e6129750
Philip Deegan <philip.deegan@gmail.com>
2015-04-09 21:00:10 +0100

    update

--- main.cpp
+++ main.cpp
@@ -21,28 +21,29 @@ Lesser General Public License for more details.
 You should have received a copy of the GNU Lesser General Public License
 along with this library.  If not, see <http://www.gnu.org/licenses/>.
 */
-#include <irrlicht.h>
 #include "driverChoice.h"
 
-#include "irrlight.hpp"
+#include "kul/log.hpp"
 
-#include <scene.hpp>
+#include "scene.hpp"
 
-int main(int argc, char* argv[]) {	
-	using namespace irrlight;
-	std::shared_ptr<SceneGraph> sg(SceneGraph::INSTANCE());
-	irr::IrrlichtDevice& device = *createDevice(video::EDT_BURNINGSVIDEO, core::dimension2d<u32>(512, 512), 32, false, false, false, sg.get());
+#ifdef _MSC_VER
+#pragma comment(lib, "Irrlicht.lib")
+#endif
+
+int main(int argc, char* argv[]) {
+	SceneGraph& sg = SceneGraph::INSTANCE();
+	irr::IrrlichtDevice& device = *createDevice(video::EDT_BURNINGSVIDEO, core::dimension2d<u32>(512, 512), 32, false, false, false, &sg);
 	//irr::IrrlichtDevice * device  = createDevice(video::EDT_OPENGL, core::dimension2d<u32>(512, 384), 32, false, false, false, SceneGraph::getInstance());
 
-	sg->device(&device);
-	sg->scene(new Scene(&device));
+	sg.device(&device);
+	sg.scene(new Scene(&device));
 
 	try{
-		while(device.run() && device.getVideoDriver())  sg->draw();		
+		while(device.run() && device.getVideoDriver())  sg.draw();
 	}
-	catch(std::exception & e) {
-		std::cout << e.what() << std::endl;
-	}
+	catch(const kul::Exception& e){ KOUT(NON) << e.what(); }
+	catch(const std::exception& e){ KOUT(NON) << e.what(); }
 
 	device.closeDevice();
 	return 0;
